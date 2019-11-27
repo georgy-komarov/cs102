4a0fbcafa7f43ce110940a68ed25a8699b28d2e6
Georgy Komarov <georgy.komarov@mail.ru>
2019-11-27 19:21:01 +0300

    Make /all command + some refactoring

--- homework05-go/bot.go
+++ homework05-go/bot.go
@@ -10,6 +10,7 @@ import (
 )
 
 var days = []string{"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"}
+var daysRussian = []string{"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"}
 var cache = make(map[string]string)
 
 func index(slice []string, s string) int {
@@ -47,7 +48,7 @@ func zip(slices ...[]string) ([][]string, error) {
 
 func getCurrentDate(group string) (string, time.Time) {
 	var scheduleWeek string
-	webPage := getPage(group, "0")
+	webPage, _ := getPage(group, "0")
 	html := soup.HTMLParse(webPage)
 
 	weekText := html.Find("h2", "class", "schedule-week").FullText()
@@ -61,21 +62,15 @@ func getCurrentDate(group string) (string, time.Time) {
 	return scheduleWeek, date
 }
 
-func getPage(group, week string) string {
+func getPage(group, week string) (string, error) {
 	if week != "" {
 		week = week + "/"
 	}
 	url := fmt.Sprintf("%[1]s/%[2]s/%[3]sraspisanie_zanyatiy_%[2]s.htm", Domain, group, week)
 	if val, ok := cache[url]; ok {
-		return val
+		return val, nil
 	} else {
-		body, err := soup.Get(url)
-		if err != nil {
-			return ""
-		} else {
-			cache[url] = body
-		}
-		return body
+		return soup.Get(url)
 	}
 }
 
@@ -116,26 +111,21 @@ func parseScheduleForDay(page string, num int) ([]string, []string, []string) {
 	return nil, nil, nil
 }
 
-func getDayScheduleText(group, week string, day int) string {
-	webPage := getPage(group, week)
-	if webPage == "" {
-		return "Ошибка получения расписания!"
-	} else {
-		times, locations, lessons := parseScheduleForDay(webPage, day)
-		if times != nil && locations != nil && lessons != nil {
-			timetable, err := zip(times, locations, lessons)
-			if err != nil {
-				return "Ошибка парсинга расписания!"
-			} else {
-				var msg string
-				for i := range timetable {
-					msg += fmt.Sprintf("<b>%s</b>; %s; %s\n", times[i], locations[i], lessons[i])
-				}
-				return msg
-			}
+func getDayScheduleText(webPage string, day int) string {
+	times, locations, lessons := parseScheduleForDay(webPage, day)
+	if times != nil && locations != nil && lessons != nil {
+		timetable, err := zip(times, locations, lessons)
+		if err != nil {
+			return "Ошибка парсинга расписания!"
 		} else {
-			return "В этот день пар нет."
+			var msg string
+			for i := range timetable {
+				msg += fmt.Sprintf("<b>%s</b>; %s; %s\n", times[i], locations[i], lessons[i])
+			}
+			return strings.TrimSpace(msg)
 		}
+	} else {
+		return "В этот день пар нет."
 	}
 }
 
@@ -172,7 +162,15 @@ func GetSchedule(update *tgbotapi.Update) {
 		command, group, week := text[0], text[1], text[2]
 		if len(group) == 5 && (week == "0" || week == "1" || week == "2") {
 			dayNum := index(days, command[1:]) + 1
-			msg = getDayScheduleText(group, week, dayNum)
+			webPage, err := getPage(group, week)
+			if err != nil {
+				msg = "Ошибка загрузки страницы!"
+			} else {
+				msg = getDayScheduleText(webPage, dayNum)
+
+			}
+		} else {
+			msg = "Некорректный ввод"
 		}
 	}
 	tgmsg := tgbotapi.NewMessage(update.Message.Chat.ID, msg)
@@ -202,7 +200,38 @@ func GetTomorrow(update *tgbotapi.Update) {
 			} else {
 				dayNum += 1
 			}
-			msg = getDayScheduleText(group, week, dayNum)
+			webPage, err := getPage(group, week)
+			if err != nil {
+				msg = "Ошибка загрузки страницы!"
+			} else {
+				msg = getDayScheduleText(webPage, dayNum)
+			}
+		}
+	}
+	tgmsg := tgbotapi.NewMessage(update.Message.Chat.ID, msg)
+	tgmsg.ParseMode = "HTML"
+	_, _ = Bot.Send(tgmsg)
+}
+
+func GetAll(update *tgbotapi.Update) {
+	var msg string
+	text := strings.Fields(update.Message.Text)
+	if len(text) != 3 {
+		msg = "Некорректный ввод"
+	} else {
+		group, week := text[1], text[2]
+		if len(group) == 5 && (week == "0" || week == "1" || week == "2") {
+			webPage, err := getPage(group, week)
+			if err != nil {
+				msg = "Ошибка загрузки страницы!"
+			} else {
+				for day := 1; day <= 7; day++ {
+					dayName := strings.Title(daysRussian[day-1])
+					msg += fmt.Sprintf("%s:\n%s\n\n", dayName, getDayScheduleText(webPage, day))
+				}
+			}
+		} else {
+			msg = "Некорректный ввод"
 		}
 	}
 	tgmsg := tgbotapi.NewMessage(update.Message.Chat.ID, msg)
@@ -226,6 +255,9 @@ func main() {
 		}
 		if IsCommand(update, "tomorrow") {
 			go GetTomorrow(update)
+		}
+		if IsCommand(update, "all") {
+			go GetAll(update)
 		}
 	}
 }
