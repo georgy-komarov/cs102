205b6723220fcabcc84230f89f8f8208e271b67e
Georgy Komarov <georgy.komarov@mail.ru>
2020-05-08 00:31:01 +0300

    Make possibility to add tags

--- homework08-elevennote/src/notes/forms.py
+++ homework08-elevennote/src/notes/forms.py
@@ -1,10 +1,46 @@
 from django import forms
 
-from .models import Note
+from .models import Note, Tag
+
 
 class NoteForm(forms.ModelForm):
-
     class Meta:
         model = Note
         fields = ['title', 'body']
 
+    field_order = ['title', 'tags', 'body']
+
+    tags = forms.CharField(label='Tags', widget=forms.TextInput(attrs={'data-role': 'tagsinput',
+                                                                       'placeholder': 'Add a tag',
+                                                                       'class': 'd-block w-25'}))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.pk is not None:
+            existing_tags = ','.join(map(str, self.instance.tags.all()))
+            if existing_tags:
+                self.fields['tags'].widget.attrs['value'] = existing_tags
+
+    def clean_tags(self):
+        tags = self.cleaned_data.get('tags')
+        tags_list = tags.split(',')
+        max_length = Tag._meta.get_field('name').max_length
+        cleaned_tags = list(filter(lambda tag: 0 < len(tag) < max_length, tags_list))
+
+        if len(cleaned_tags) != len(tags_list):
+            raise forms.ValidationError(f'Tag name must not exceed {max_length} characters! '
+                                        f'Please, remove these tags: {", ".join(set(tags_list) - set(cleaned_tags))}')
+        return cleaned_tags
+
+    def save(self, commit=True):
+        instance = super().save(commit=commit)
+
+        tags = self.cleaned_data.get('tags')
+        instance.tags.clear()
+
+        for tag in tags:
+            tag, created = Tag.objects.get_or_create(name=tag)
+            instance.tags.add(tag)
+
+        instance.save()
+        return instance
--- homework08-elevennote/src/notes/models.py
+++ homework08-elevennote/src/notes/models.py
@@ -5,11 +5,19 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Note(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     pub_date = models.DateTimeField('date published', auto_now_add=True)
     owner = models.ForeignKey(User, related_name='notes', on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, related_name='notes')
 
     def was_published_recently(self):
         now = timezone.now()
